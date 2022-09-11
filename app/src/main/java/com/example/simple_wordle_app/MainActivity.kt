b2d190fe797ce59f91bff175bd2b91043c858a94
Kouko <lyeaxe1005@gmail.com>
2022-09-10 17:12:44 -0700

    Added Option To Type A Word

--- app/src/main/java/com/example/simple_wordle_app/MainActivity.kt
+++ app/src/main/java/com/example/simple_wordle_app/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.simple_wordle_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var guessButton: Button
+    lateinit var guessText: EditText
+    lateinit var first: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        guessText = findViewById(R.id.guessText)
+        guessButton = findViewById(R.id.guessButton)
+        first = findViewById(R.id.one)
+
+        guessButton.setOnClickListener {
+            var word = guessText.text
+            first.text = word
+            guessText.setText("")
+        }
+
     }
 }
